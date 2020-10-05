54ce52586d823dff9d146d2ab531a847255d5715
BEATT <beattyjoshua55@Gmail.com>
2020-10-05 22:09:40 +0100

    outated file removed

--- envs/environments.js
+++
@@ -1,8 +0,0 @@
-const PropertiesReader = require('properties-reader');
-const prop = PropertiesReader('./bot.properties');
-
-let getProperty = function(pty){
-    return prop.get(pty);
-}
-
-module.exports = getProperty
